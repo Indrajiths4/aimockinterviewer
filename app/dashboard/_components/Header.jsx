c68ae9679a75caafb93d201264593b336ec9a3d6
Indrajiths4 <indrajithmundackal@gmail.com>
2024-12-08 15:27:18 +0530

    Second commit

--- app/dashboard/_components/Header.jsx
+++ app/dashboard/_components/Header.jsx
@@ -9,7 +9,11 @@ function Header() {
     const path = usePathname();
   return (
     <div className='flex p-4 items-center justify-between bg-secondary shadow-sm'>
-        <Image src={'/logo.svg'} width={160} height={100} alt='Logo'></Image>
+        {/* <Image src={'/logo.svg'} width={160} height={100} alt='Logo'></Image> */}
+        <div className='flex items-center gap-2'>
+                <Image src='/ailogo.png' width={40} height={40} alt='AI Logo' className='object-contain' />
+                <span className='text-xl font-semibold'>AIInterviewer</span>
+            </div>
         <ul className='hidden md:flex gap-6'>
             <li className={`hover:text-primary hover:font-bold transition-all cursor-pointer ${path=='/dashboard'&&'text-primary font-bold'}`}>Dashboard</li>
             <li className={`hover:text-primary hover:font-bold transition-all cursor-pointer ${path=='/dashboard/questions'&&'text-primary font-bold'}`}>Questions</li>
